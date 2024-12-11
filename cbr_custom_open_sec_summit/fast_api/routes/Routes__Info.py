a1ab6ae6cadd2519eb1fd4a30d3a28f245196c41
Dinis Cruz <dinis.cruz@owasp.org>
2024-12-11 16:58:55 +0000

    fixed ping path

--- cbr_custom_open_sec_summit/fast_api/routes/Routes__Info.py
+++ cbr_custom_open_sec_summit/fast_api/routes/Routes__Info.py
@@ -1,7 +1,7 @@
 from osbot_fast_api.api.Fast_API_Routes  import Fast_API_Routes
 from cbr_custom_open_sec_summit.utils.Version import version__cbr_custom_open_sec_summit
 
-ROUTES_PATHS__INFO = ['/info/version']
+ROUTES_PATHS__INFO = ['/info/version',  '/info/ping']
 
 class Routes__Info(Fast_API_Routes):
     tag :str = 'info'
